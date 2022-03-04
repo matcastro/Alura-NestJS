8d4ebec32d2594f6aab01d68f2d9eac878e81b8d
Mateus Castro <matcastro2004@yahoo.com.br>
2022-03-04 10:48:04 -0300

    Usando filtros e interceptors para serialização e tratamento de erros.

--- nest-avancado/src/app.module.ts
+++ nest-avancado/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { FiltroDeExcecaoHttp } from './common/filtros/filtro-de-excecao-http.filter';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [UsuarioModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: FiltroDeExcecaoHttp
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor
+    }
+  ],
 })
 export class AppModule {}
--- nest-avancado/src/usuario/usuario.entity.ts
+++ nest-avancado/src/usuario/usuario.entity.ts
@@ -1,9 +1,13 @@
+import { Exclude, Expose } from "class-transformer"
 import { IsEmail, IsNotEmpty, IsString } from "class-validator"
 import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico.validator"
 
 export class Usuario {
     id: number
 
+    @Expose({
+        name: 'username'
+    })
     @IsNomeDeUsuarioUnico({
         message: 'nomeDeUsuario precisa ser único'
     })
@@ -20,14 +24,27 @@ export class Usuario {
     })
     email: string
 
+    @Expose({
+        name: 'password'
+    })
+    @Exclude({
+        toPlainOnly: true
+    })
     @IsNotEmpty({
         message: 'senha é obrigatório'
     })
     senha: string
 
+    @Expose({
+        name: 'fullName'
+    })
     @IsNotEmpty({
         message: 'nomeCompleto é obrigatório'
     })
     nomeCompleto: string
+
+    @Expose({
+        name: 'joinDate'
+    })
     dataDeEntrada: Date
 }
