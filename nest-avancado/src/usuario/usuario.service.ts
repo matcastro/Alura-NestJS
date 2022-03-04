2b645b6833c17d34338f9130816fd1cac951a741
Mateus Castro <matcastro2004@yahoo.com.br>
2022-03-04 09:36:25 -0300

    Realizando injeção de dependência. Tipando usuário com uma entity e separando módulos por entidade

--- nest-avancado/src/usuario/usuario.service.ts
+++ nest-avancado/src/usuario/usuario.service.ts
@@ -1,8 +1,24 @@
+import { Injectable } from "@nestjs/common"
+import { Usuario } from "./usuario.entity"
+
+@Injectable()
 export class UsuarioService {
 
-    private usuarios = []
+    private usuarios: Usuario[] = [{
+        id: 1,
+        nomeDeUsuario: 'mateus',
+        email: 'mateus.ribeiro@fazendo.curso',
+        senha: '123456',
+        nomeCompleto: 'Mateus Ribeiro',
+        dataDeEntrada: new Date()
+    }]
 
-    public cria(usuario){
+
+    public buscaPorNomeDeUsuario(nomeDeUsuario: string): Usuario{
+        return this.usuarios.find(usuario => usuario.nomeDeUsuario === nomeDeUsuario)
+    }
+
+    public cria(usuario: Usuario): Usuario{
         this.usuarios.push(usuario)
         return usuario
     }
