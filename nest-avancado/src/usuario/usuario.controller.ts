1aecdd188bae8a95361a0b50de1003a3cda9eb39
Mateus Castro <matcastro2004@yahoo.com.br>
2022-03-04 11:23:38 -0300

    Interceptor para tratamento de resposta sem perder as funcionalidades do interceptor do Nest. Abstração da resposta através da classe NestResponse

--- nest-avancado/src/usuario/usuario.controller.ts
+++ nest-avancado/src/usuario/usuario.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common"
+import { Body, Controller, Get, HttpStatus, NotFoundException, Param, Post } from "@nestjs/common"
+import { NestResponse } from "../core/http/nest-response"
+import { NestResponseBuilder } from "../core/http/nest-response-builder"
 import { Usuario } from "./usuario.entity"
 import { UsuarioService } from "./usuario.service"
 
@@ -9,12 +11,27 @@ export class UsuarioController {
     @Get(':nomeDeUsuario')
     public buscaPorNomeDeUsuario(@Param('nomeDeUsuario') nomeDeUsuario: string): Usuario{
         const usuarioEncontrado = this.usuarioService.buscaPorNomeDeUsuario(nomeDeUsuario)
+
+        if(!usuarioEncontrado){
+            throw new NotFoundException({
+                satusCode: HttpStatus.NOT_FOUND,
+                message: `Usuário ${nomeDeUsuario} não foi encontrado`
+            })
+        }
+
         return usuarioEncontrado
     }
 
     @Post()
-    public cria(@Body() usuario: Usuario): Usuario{
+    public cria(@Body() usuario: Usuario): NestResponse{
         const usuarioCriado = this.usuarioService.cria(usuario)
-        return usuarioCriado
+
+        return new NestResponseBuilder()
+            .comStatus(HttpStatus.CREATED)
+            .comHeaders({
+                'Location': `/users/${usuarioCriado.nomeDeUsuario}`
+            })
+            .comBody(usuarioCriado)
+            .build()
     }
 }
