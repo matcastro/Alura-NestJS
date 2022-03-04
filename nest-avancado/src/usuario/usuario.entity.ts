f26e37a537d991f0f1a3c1117c16da549eed725a
Mateus Castro <matcastro2004@yahoo.com.br>
2022-03-04 10:14:15 -0300

    Validações padrão com class-validator, validações customizadas, injeção de dependências em decorators de validação.

--- nest-avancado/src/usuario/usuario.entity.ts
+++ nest-avancado/src/usuario/usuario.entity.ts
@@ -1,8 +1,33 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+import { IsNomeDeUsuarioUnico } from "./is-nome-de-usuario-unico.validator"
+
 export class Usuario {
     id: number
+
+    @IsNomeDeUsuarioUnico({
+        message: 'nomeDeUsuario precisa ser único'
+    })
+    @IsNotEmpty({
+        message: 'nomeDeUsuario é obrigatório'
+    })
+    @IsString({
+        message: 'nomeDeUsuario precisa ser uma string'
+    })
     nomeDeUsuario: string
+
+    @IsEmail({}, {
+        message: 'email precisa ser um endereço de email'
+    })
     email: string
+
+    @IsNotEmpty({
+        message: 'senha é obrigatório'
+    })
     senha: string
+
+    @IsNotEmpty({
+        message: 'nomeCompleto é obrigatório'
+    })
     nomeCompleto: string
     dataDeEntrada: Date
 }
